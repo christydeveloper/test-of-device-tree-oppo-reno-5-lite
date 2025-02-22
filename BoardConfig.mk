bd27a32bf7d786e56a88ba12e3420a27f60429d3
christydeveloper <64496053+christydeveloper@users.noreply.github.com>
2025-02-22 10:38:24 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := device/OPLUS/CPH2205
+LOCAL_PATH := OSSIS/OPLUS/CPH2205
 
 TARGET_BOARD_PLATFORM := mt6779               # From ro.mediatek.platform, but lowercase value
 TARGET_NO_BOOTLOADER := true
