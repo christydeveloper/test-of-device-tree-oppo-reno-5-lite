23fba750704f5f2ff3624951dbc089dc215307c8
christydeveloper <64496053+christydeveloper@users.noreply.github.com>
2025-02-25 07:38:52 -0600

    Update omni_CPH2205.mk

--- omni_CPH2205.mk
+++ omni_CPH2205.mk
@@ -1,21 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Add this line if your device is 64-bit
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-# Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Another common config inclusion
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
-
-# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Replace ${_CODENAME_} with your Device Name's Value.
-# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
-PRODUCT_COPY_FILES += device/${_VENDORNAME_}/${_CODENAME_}/prebuilt/kernel:kernel
-# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
-# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+PRODUCT_COPY_FILES += device/0SSI/CPH2205/prebuilt/kernel:kernel
 
 PRODUCT_DEVICE := CPH2205
 PRODUCT_NAME := omni_OSSI
